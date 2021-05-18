8016adb54ac03fc1d418049423465111a1fc2cc7
ethanmarkowski <55117285+ethanmarkowski@users.noreply.github.com>
2021-05-18 12:20:01 -0400

    creating lab 1

---
+++ Lab1_ButtonsAndLeds/Lab1_ButtonsAndLeds/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lab1_ButtonsAndLeds.cpp
+ *
+ * Created: 5/18/2021 12:00:12 PM
+ * Author : ethan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
