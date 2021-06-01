6bde0368344d22462e89e1f020094d962326022b
ethanmarkowski <55117285+ethanmarkowski@users.noreply.github.com>
2021-06-01 02:21:26 -0400

    creating lab 2 files

---
+++ Lab2_TimingAndDelays/Lab2_TimingAndDelays/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Lab2_TimingAndDelays.cpp
+ *
+ * Created: 6/1/2021 2:07:29 AM
+ * Author : Ethan Markowski
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
