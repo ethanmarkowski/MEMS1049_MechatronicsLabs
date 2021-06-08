d08c2201d4c59f763e1ceaf3b69fbdbe4d5b24ea
ethanmarkowski <55117285+ethanmarkowski@users.noreply.github.com>
2021-06-08 04:58:33 -0400

    creating lab 3 project

---
+++ Lab 3 - Interrupts/Lab 3 - Interrupts/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lab 3 - Interrupts.cpp
+ *
+ * Created: 6/7/2021 4:57:04 PM
+ * Author : Ethan Markowski
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
